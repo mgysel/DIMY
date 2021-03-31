216550f56ef57b2e378000d36a1e140cadea6528
William [z5017279] <z5017279@ad.unsw.edu.au>
2021-03-31 14:09:03 +1100

    Added arguments. Refactored the code a bit so that each task is its own function. Added simple interface for running tasks.

--- DIMY.py
+++ DIMY.py
@@ -25,8 +25,11 @@ def genEphID():
     print(f"Ephemeral ID: {ephID}")
     return ephID
 
+ephID = None
 # Test
-ephID = genEphID()
+def task1():
+    global ephID
+    ephID = genEphID()
 
 # Task 2: Prepare n shares of the EphID by using k-out-of-n Shamir Secret Sharing mechanism. 
 # For this implementation, we use the values of k and n to be 3 and 6 respectively.
@@ -39,28 +42,16 @@ def genShares(ephID):
     print(f"Shares: {shares}")
     return shares
 
+shares = None
 # Test
-shares = genShares(ephID)
+def task2():
+    global shares
+    shares = genShares(ephID)
 
 # Task 3: Broadcast these n shares @ 1 unique share per 10 seconds. 
 # For this implementation, you do not need to implement the simultaneous advertisement of EphIDs proposed in the reference paper [1].
-
-
 ########## SENDER ##########
 # UDP socket programming references https://github.com/ninedraft/python-udp
-
-# Create UDP socket for sender
-server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-# Enable port reusage so we can run multiple clients/servers on single (host/port)
-server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
-# Enable broadcasting mode
-server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-
-# Set a timeout so the socket does not block indefinitely when trying to receive data.
-server.settimeout(0.2)
-# Bind socket to localhost port 44444
-server.bind(("", 44444))
-
 def user_send(ephID):
     '''
     User broadcasts one share of the EphID every 10 seconds to another user
@@ -93,23 +84,7 @@ def user_send(ephID):
         # TODO - UPDATE TO 10 SECONDS, 1 SECOND BETTER FOR TESTING
         time.sleep(1)
 
-# Create thread for user to broadcast chunks of the EphID
-message = ephID
-send_broadcast = threading.Thread(target=user_send, args=(ephID,))
-send_broadcast.start()
-
-
 ########## RECEIVER ##########
-
-# Create UDP socket for receiver
-client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-# Enable port reusage so we will be able to run multiple clients and servers on single (host, port).
-client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
-# Enable broadcasting mode
-client.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-# Bind socket to localhost port 37024
-client.bind(("", 37025))
-
 def user_receive():
     '''
     User receives broadcast from another user
@@ -151,27 +126,132 @@ def user_receive():
             print(hashlib.sha256(ephID))
             print(hash_ephID)
 
-# Create thread for user to receive broadcasts
-receive_broadcast = threading.Thread(target=user_receive)
-receive_broadcast.start()
+def task3():
+    ########## SENDER ##########
+    # UDP socket programming references https://github.com/ninedraft/python-udp
+
+    # Create UDP socket for sender
+    server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    # Enable port reusage so we can run multiple clients/servers on single (host/port)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+    # Enable broadcasting mode
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+    # Set a timeout so the socket does not block indefinitely when trying to receive data.
+    server.settimeout(0.2)
+    # Bind socket to localhost port 44444
+    server.bind(("", 44444))
+
+    # Create thread for user to broadcast chunks of the EphID
+    message = ephID
+    send_broadcast = threading.Thread(target=user_send, args=(ephID,))
+    send_broadcast.start()
+
+
+    ########## RECEIVER ##########
+
+    # Create UDP socket for receiver
+    client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    # Enable port reusage so we will be able to run multiple clients and servers on single (host, port).
+    client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+    # Enable broadcasting mode
+    client.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    # Bind socket to localhost port 37024
+    client.bind(("", 37025))
+
+    # Create thread for user to receive broadcasts
+    receive_broadcast = threading.Thread(target=user_receive)
+    receive_broadcast.start()
 
 # Task 4: 4-A Show the devices attempting re-construction of EphID when these have received at least 3 shares.
 # Task 4: 4-B Show the devices verifying the re-constructed EphID by taking the hash of re-constructed EphID and comparing with the hash value received in the advertisement.
+def task4():
+    pass
 
 # Task 5: 5-A Show the devices computing the shared secret EncID by using Diffie- Hellman key exchange mechanism.
 # Task 5: 5-B Show that the devices have arrived at the same EncID value.
+def task5():
+    pass
 
 # Task 6: Show that the devices are encoding EncID into the DBF and deleting the EncID.
+def task6():
+    pass
 
 # Task 7: 7-A Show that the devices are encoding multiple EncIDs into the same DBF and show the state of the DBF after each addition.
 # Task 7: 7-B Show that a new DBF gets created for the devices after every 10 minutes. A device can only store maximum of 6 DBFs.
+def task7():
+    pass
 
 # Task 8: Show that after every 60 minutes, the devices combine all the available DBFs into a single QBF.
+def task8():
+    pass
 
 # Task 9: 9-A Show that the devices send the QBF to the back-end server. For extension, the back-end server is your own centralised server.
 # Task 9: 9-B Show that the devices are able to receive the result of risk analysis back from the back-end server. Show the result for a successful as well as an unsuccessful match. For extension, the back-end server is your own centralised server.
+def task9():
+    pass
 
 # Task 10: Show that a device can combine the available DBF into a CBF and upload the CBF to the back-end server. For extension, the back-end server is your own centralised server.
+def task10():
+    pass
 
 # Task 11: 11-A Show that the device is able to establish a TCP connection with the centralised server and perform Tasks 9 and 10 successfully.
 # Task 11: 11-B Show the terminal for the back-end server performing the QBF-CBF matching operation for risk analysis.
+def task11():
+    pass
+
+def task12():
+    pass
+
+def handle_args():
+    import argparse
+    parser = argparse.ArgumentParser(description="Runner script for DIMY assignment")
+    parser.add_argument("task", type=int, nargs="*", default=99, help="Task number to run.")
+    parser.add_argument("--port", "-p", type=int, action="store", nargs=2, help="Port number to run client/server on.")
+
+    args = parser.parse_args()
+    
+    return args.task, (args.port if args.port else None)
+
+
+tasks = [
+    task1,
+    task2,
+    task3,
+    task4,
+    task5,
+    task6,
+    task7,
+    task8,
+    task9,
+    task10,
+    task11,
+    task12,
+]
+
+if __name__ == "__main__":
+    task, empty = handle_args()
+
+    if len(task) == 1:
+        task = task[0]
+    else:
+        for i in task:
+            tasks[i]()
+
+    if task > len(tasks):
+        for i, f in enumerate(tasks):
+            f()
+    else:
+        i = 0
+        while i < task:
+            tasks[i]()
+        # tasks[task - 1]()
+    while True:
+        try:
+            num = input("Enter number to run that task. EOF to end.")
+            num = int(num)
+            i = 0
+            while i < num:
+                tasks[i]()
+        except EOFError:
+            break
