f8fdcb118042ffed1811dc9c92315e09fb56cfbc
William [z5017279] <z5017279@ad.unsw.edu.au>
2021-04-06 18:48:39 +1000

    Needs more mulling.

--- DIMY.py
+++ DIMY.py
@@ -20,7 +20,105 @@ from ecdsa import ECDH, SECP128r1, VerifyingKey
 # from bloom_filter import BloomFilter
 # from pybloomfilter import BloomFilter
 import bitarray
+import mmh3
+import math
 from Crypto.Random.random import getrandbits
+
+# Nicked the base from https://www.geeksforgeeks.org/bloom-filters-introduction-and-python-implementation/
+class BloomFilter(object):
+    '''
+    Class for Bloom filter, using murmur3 hash function
+    '''
+    # p
+    fb_prob = None
+    # n
+    size = None
+    # k
+    hash_count = None
+    # m
+    bit_array = None
+
+    def __init__(self, size=800000, items_count=1000, fp_prob=0.0000062, num_hashes=3):
+        '''
+        items_count : int
+            Number of items expected to be stored in bloom filter
+        fp_prob : float
+            False Positive probability in decimal
+        '''
+        # False posible probability in decimal
+        self.fp_prob = fp_prob
+
+        # Size of bit array to use
+        self.size = self.get_size(items_count, fp_prob)
+
+        # number of hash functions to use
+        self.hash_count = self.get_hash_count(self.size, items_count)
+
+        # Bit array of given size
+        self.bit_array = bitarray(self.size)
+
+        # initialize all bits as 0
+        self.bit_array.setall(0)
+
+    def add(self, item):
+        '''
+        Add an item in the filter
+        '''
+        digests = []
+        for i in range(self.hash_count):
+
+            # create digest for given item.
+            # i work as seed to mmh3.hash() function
+            # With different seed, digest created is different
+            digest = mmh3.hash(item, i) % self.size
+            digests.append(digest)
+
+            # set the bit True in bit_array
+            self.bit_array[digest] = True
+
+    def check(self, item):
+        '''
+        Check for existence of an item in filter
+        '''
+        for i in range(self.hash_count):
+            digest = mmh3.hash(item, i) % self.size
+            if self.bit_array[digest] == False:
+
+                # if any of bit is False then,its not present
+                # in filter
+                # else there is probability that it exist
+                return False
+        return True
+
+    @classmethod
+    def get_size(self, n, p):
+        '''
+        Return the size of bit array(m) to used using
+        following formula
+        m = -(n * lg(p)) / (lg(2)^2)
+        n : int
+            number of items expected to be stored in filter
+        p : float
+            False Positive probability in decimal
+        '''
+        m = -(n * math.log(p))/(math.log(2)**2)
+        return int(m)
+
+    @classmethod
+    def get_hash_count(self, m, n):
+        '''
+        Return the hash function(k) to be used using
+        following formula
+        k = (m/n) * lg(2)
+
+        m : int
+            size of bit array
+        n : int
+            number of items expected to be stored in filter
+        '''
+        k = (m/n) * math.log(2)
+        return int(k)
+
 
 
 # Threading
@@ -361,9 +459,9 @@ def task6(EncID=None):
     seed2 = getrandbits(32)
     seed3 = getrandbits(32)
     # ! May need to move this to global depending on how everything flows.
-    # instantiates bloom filter with n=1000, m=800000 bits and a false positive rate of p=0.0000062, k=2 hashes
+    # instantiates bloom filter with n=1000, m=800000 bits and a false positive rate of p=0.0000062, k=3 hashes
     # * may need to decide filename. filename=""
-    daily_bloom_filter = BloomFilter(capacity=1000, error_rate=0.0000062, hashseeds=[seed1, seed2, seed3])
+    daily_bloom_filter = bitarray.bitarray(800000)
     print(daily_bloom_filter.capacity)
     print("number bits: " + daily_bloom_filter.num_bits_m)
     print("error rate: " + daily_bloom_filter.error_rate_p)
