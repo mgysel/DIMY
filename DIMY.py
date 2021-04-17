62575302112fd545e8cdfd8357affe518d1a4966
Mike Gysel <z5251938@ad.unsw.edu.au>
2021-04-18 09:13:10 +1000

    Formatted Segments 1-6

--- DIMY.py
+++ DIMY.py
@@ -272,18 +272,18 @@ from json import dumps
 server = None
 client = None
 
-# Variable to hold shares along with hash of EphID
 
 
 
-# Task 1: Generate a 16-Byte Ephemeral ID (EphID) after every 1 minute.
-ecdh = None
+############################## Task 1/2 ##############################
+# Every 60 seconds, generate new EphID and associated shares
+
 ephID = None
-hash_ephID = None
+ecdh = None
 def genEphID():
     '''
-    Generates a 16-Byte Ephemeral ID and hash of that Ephemeral ID
-    Repeats every minute
+    Generates a 16 Byte ephemeral ID using ECDH
+    Stores in global ephID variable
     '''
     global ecdh
     global ephID
@@ -291,77 +291,88 @@ def genEphID():
     
     ecdh = ECDH(curve=SECP128r1)
 
+    ecdh.generate_private_key()
+    public_key = ecdh.get_public_key()
+    ephID = public_key.to_string('compressed')[1:]
+
+hash_ephID = None
+def genHashEphID():
+    '''
+    Generates a hash of the ephemeral ID
+    Stores hash in global hash_ephID variable
+    '''
+    global hash_ephID
+
+    hash_ephID = hashlib.sha256(ephID).hexdigest()
+
+# Variable to hold shares, hash of EphID, temporarily store Ephemeral ID
+send_shares = None
+def genShares():
+    '''
+    Generates n shares of the EphID by using k-out-of-n Shamir Secret Sharing mechanism
+    k = 3, n = 6
+    Stores shares in global send_shares variable
+    '''
+    global send_shares
+
+    send_shares = Shamir.split(3, 6, ephID)
+
+def genEphIDHashShares():
+    '''
+    Generates a 16-Byte Ephemeral ID, hash of the Ephemeral ID, and Shamir Secret Shares
+    Repeats every minute
+    Stores in global ephID, hash_ephID, send_shares variables
+    '''
+    global ephID
+    global hash_ephID
+    global send_shares
+
     while (True):
-        ecdh.generate_private_key()
-        public_key = ecdh.get_public_key()
-        ephID = public_key.to_string('compressed')[1:]
-        hash_ephID = hashlib.sha256(ephID).hexdigest()
-        # print(f"public key: {public_key.to_string('compressed')}")
-        # print(f"Ephemeral ID: {ephID}")
-        # print(f"Number of Bytes: {len(ephID)}")
+        genEphID()
+        genHashEphID()
+        genShares()
+
         print("\n------------------> Segment 1 <------------------")
         print(f"generate EphID: {ephID}")
         print(f"hash value of EphID: {hash_ephID}\n")
 
-        # Whenever new Ephemeral ID is created, create shares  of that Ephemeral ID
-        task2()
+        print("------------------> Segment 2 <------------------")
+        print("[")
+        for share in send_shares:
+            print(f"\t{share[1]}")
+        print("]")
 
         time.sleep(60)
 
-secret = None
-# Test
-def task1():
-    global ephID
-
-    # Create thread to generate an ephID every minute
-    create_ephID = threading.Thread(target=genEphID, args=())
-    create_ephID.start()
-
-# Task 2: Prepare n shares of the EphID by using k-out-of-n Shamir Secret Sharing mechanism. 
-# For this implementation, we use the values of k and n to be 3 and 6 respectively.
-def genShares(ephID):
-    '''
-    Prepares 6 chunks of an ephemeral ID (ephID)
-    Returns list of 6 chunks
-    '''
-    shares = Shamir.split(3, 6, ephID)
-    return shares
-
-shares = None
-# Test
-def task2():
-    global shares
-    shares = genShares(ephID)
-
-    print("------------------> Segment 2 <------------------")
-    #print(f"Shares: {shares}")  
-    print("[")
-    for share in shares:
-        print(f"\t{share[1]}")
-    print("]")
-
-    task3()
-      
+# Start thread to generate ephID, hash, and shares every minute
+create_ephID = threading.Thread(target=genEphIDHashShares, args=())
+create_ephID.start()
 
 
-# Task 3: Broadcast these n shares @ 1 unique share per 10 seconds. 
-# For this implementation, you do not need to implement the simultaneous advertisement of EphIDs proposed in the reference paper [1].
-########## SENDER ##########
-# UDP socket programming references https://github.com/ninedraft/python-udp
-def user_send(ephID):
+
+
+############################## TASK 3 ##############################
+# Send and receive shares
+
+# Task 3A: Broadcast n shares at rate of 1 unique share per 10 seconds. 
+# References UDP socket programming https://github.com/ninedraft/python-udp
+def user_send():
     '''
     User broadcasts one share of the EphID every 10 seconds to another user
     '''
-    # Determine Hash of EphID
-    hash_ephID = hashlib.sha256(ephID).hexdigest()
+    # # Determine Hash of EphID
+    # hash_ephID = hashlib.sha256(ephID).hexdigest()
 
-    # Determine shares of EphID
-    ephID_shares = genShares(ephID)
+    # # Determine shares of EphID
+    # ephID_shares = genShares(ephID)
+    global ephID
+    global hash_ephID
+    global send_shares
 
     i = 0
     while True:
         # Convert share to bytes
-        share = (ephID_shares[i][0], binascii.hexlify(ephID_shares[i][1]), hash_ephID)
+        share = (send_shares[i][0], binascii.hexlify(send_shares[i][1]), hash_ephID)
         share_bytes = str.encode(str(share))
 
         print(f"\n[ Segment 3-A, sending share: {share[1]} ]")
@@ -372,18 +383,19 @@ def user_send(ephID):
         server.sendto(share_bytes, ('<broadcast>', 37025))
 
         # Increment to next share
-        if (i < 5):
-            i += 1
-        else:
+        if (i == 5):
             i = 0
+        else:
+            i += 1
 
         # Send every 10 seconds
         time.sleep(10)
 
-########## RECEIVER ##########
-def add_share(rec_hash, rec_share):
+# Task 3-B: Receive shares broadcasted by other device
+recv_shares = None
+def add_share(recv_hash, recv_share):
     '''
-    Adds a share (share_num, share_bytes) to the receiver's global shares variable
+    Adds a share (share_num, share_bytes) to the global recv_shares variable
     '''
     # Shares data structure should look like:
     # shares = [
@@ -393,7 +405,7 @@ def add_share(rec_hash, rec_share):
     #         "ephID": None
     #     }
     # ]
-    global shares
+    global recv_shares
 
     # print("********** INSIDE ADD_SHARE **********")
     # print(f"rec_hash: {rec_hash}")
@@ -401,41 +413,53 @@ def add_share(rec_hash, rec_share):
 
     is_hash_in_shares = False
 
-    for share in shares:
+    for share in recv_shares:
         # Check if hash is already in shares
-        if share['hash'] == rec_hash:
+        if share['hash'] == recv_hash:
             is_hash_in_shares = True
             # If hash already in shares, append non-duplicate shares
-            if rec_share not in share['shares']:
-                share['shares'].append(rec_share)
+            if recv_share not in share['shares']:
+                share['shares'].append(recv_share)
     
     if not is_hash_in_shares:
         # If hash not in shares, create new object with this share
-        shares.append(
+        recv_shares.append(
             {
-                "hash": rec_hash,
-                "shares": [rec_share],
+                "hash": recv_hash,
+                "shares": [recv_share],
                 "ephID": None
             }
         )
 
-def add_eph_id_to_shares(rec_hash, rec_ephID):
+def add_eph_id_to_shares(recv_hash, recv_ephID):
     '''
     Adds ephID to global shares variable
     After ephID is reconstructed
     '''
-    global shares
+    global recv_shares
 
-    for share in shares:
-        if share['hash'] == rec_hash:
-            share['ephID'] = rec_ephID
+    for share in recv_shares:
+        if share['hash'] == recv_hash:
+            share['ephID'] = recv_ephID
+
+def num_shares_received(recv_hash):
+    '''
+    Determines number of unique shares received for a given hash of an EphID
+    '''
+    global recv_shares
+
+    for share in recv_shares:
+        if share['hash'] == recv_hash:
+            return len(share['shares'])
+
+    return 0
 
 def user_receive():
     '''
     User receives broadcast from another user
     '''
-    global shares
-    shares = []
+    global recv_shares
+    recv_shares = []
     hash_ephID = None
 
     while True:
@@ -456,7 +480,8 @@ def user_receive():
 
         # print("********** Task 3B: Show the receiving of shares **********")
         # print(f"Received Share: {share}")
-        print(f"[ Segment 3-B, received share: {share[1]} ]")
+        
+        print(f"[ Segment 3-B, received share for hash {hash_ephID}: {share[1]} ]")
         
         # Add to shares
         add_share(hash_ephID, share)
@@ -464,14 +489,17 @@ def user_receive():
         # print(shares)
         # print("********** Task 3C: Keeping track of shares received **********")
         # print(f"Num unique shares received from sender: {num_shares_received(hash_ephID)}")
-        print(f"[ Segment 3-C, total shares received: {num_shares_received(hash_ephID)} ]")
+        print(f"[ Segment 3-C, total shares received for hash {hash_ephID}: {num_shares_received(hash_ephID)} ]")
 
-        # Task 4: If have 3 shares, reconstruct ephID and check hash
-        task4(hash_ephID)
+        # Task 4: If have 3 shares for that hash and ephID not reconstructed for that hash then
+        # reconstruct ephID and check hash
+        if has_k_shares(3, hash_ephID):
+            reconstruct_verify_ephID(hash_ephID)
 
-def task3():
+def start_send_recv_threads():
     global server
     global client
+    
     ########## SENDER ##########
     # UDP socket programming references https://github.com/ninedraft/python-udp
 
@@ -490,9 +518,8 @@ def task3():
     print("\n------------------> Segment 3 <------------------")
     # Create thread for user to broadcast chunks of the EphID
     message = ephID
-    send_broadcast = threading.Thread(target=user_send, args=(ephID,))
+    send_broadcast = threading.Thread(target=user_send)
     send_broadcast.start()
-
 
     ########## RECEIVER ##########
 
@@ -509,45 +536,41 @@ def task3():
     receive_broadcast = threading.Thread(target=user_receive)
     receive_broadcast.start()
 
+# Start sending shares and receiving them
+start_send_recv_threads()
+
+
+
+
+############################## TASK 4 ##############################
+# Reconstruct ephID and verify
+
 # Task 4: 4-A Show the devices attempting re-construction of EphID when these have received at least 3 shares.
 # Task 4: 4-B Show the devices verifying the re-constructed EphID by taking the hash of re-constructed EphID and comparing with the hash value received in the advertisement.
 
-def num_shares_received(rec_hash):
-    '''
-    Determines number of unique shares received for a given hash of an EphID
-    '''
-    global shares
-
-    for share in shares:
-        if share['hash'] == rec_hash:
-            return len(share['shares'])
-
-    return 0
-
-def has_k_shares(k, rec_hash):
+def has_k_shares(k, recv_hash):
     '''
     Determines if the receiver has enough of rec_hash shares 
     to reconstruct the sender's EphID
     and if the EphID was not already reconstructed
     '''
-    global shares
+    global recv_shares
 
-    for share in shares:
-        if share['hash'] == rec_hash:
+    for share in recv_shares:
+        if share['hash'] == recv_hash:
             if share['ephID'] is None:
                 return len(share['shares']) >= k
 
     return False
 
-
 def reconstruct_eph_id(rec_hash):
     '''
     Reconstructs a sender's ephID from the received shares
     '''
-    global shares
+    global recv_shares
     ephID = None
 
-    for share in shares:
+    for share in recv_shares:
         if share['hash'] == rec_hash:
             ephID = Shamir.combine(share['shares'])
     
@@ -560,8 +583,12 @@ def verify_eph_id(ephID, hash_ephID):
     '''
     return hashlib.sha256(ephID).hexdigest() == hash_ephID
 
-def task4(hash_ephID=None):
-    global shares
+def reconstruct_verify_ephID(hash_ephID=None):
+    '''
+    Reconstructs an ephID from atleast 3 shares
+    Verifies hash of that ephID with the hash sent
+    '''
+    global recv_shares
 
     # Task 4: 4-A Show the devices attempting re-construction of EphID 
     # when these have received at least 3 shares.
@@ -585,19 +612,21 @@ def task4(hash_ephID=None):
         # Store ephID in shares variable
         add_eph_id_to_shares(hash_ephID, ephID)
 
-        # Task 5
-        task5(ephID)
+        # Once we have reconstructed Ephemeral ID, compute the Encounter ID
+        construct_encID(ephID)
 
-    
-# Task 5: 5-A Show the devices computing the shared secret EncID by using Diffie- Hellman key exchange mechanism.
+
+
+
+############################## TASK 5 ##############################
+# Compute EncID
+
+# Task 5: 5-A Show the devices computing the shared secret EncID by using Diffie-Hellman key exchange mechanism.
 # Task 5: 5-B Show that the devices have arrived at the same EncID value.
-encID = None
-
 def construct_encID(ephID):
     '''
     Computes encID given an ephID
     '''
-    global encID
     global ecdh
 
     # Need to add 2 or 3 to the beginning of EphID
@@ -607,30 +636,22 @@ def construct_encID(ephID):
     ecdh.load_received_public_key_bytes(ephID)
     encID = ecdh.generate_sharedsecret_bytes()
 
-    return encID
-
-
-def task5(ephID=ephID):
-    '''
-    Computes EncID for a given EphID
-    '''
-
-    global encID
-
-    encID = construct_encID(ephID)
-
     print("\n------------------> Segment 5 <------------------")
     #print("********** Task 5A: Show the devices computing the shared secret EncID **********")
     print(f"[ generate shared secret EncID: {encID} ]")
 
     # Now encode EncID into a bloom filter called Daily Bloom Filter (DBF) and delete the EncID
     task6(encID)
+
     
 
 
+############################## TASK 6 ##############################
+# Encode EncID into the Daily Bloom Filter
+
 daily_bloom_filter = None
 # Task 6: Show that the devices are encoding EncID into the DBF and deleting the EncID.
-def task6(EncID=None):
+def task6(EncID):
     '''
     Show that the devices are encoding EncID into the DBF and deleting the EncID.
     '''
@@ -649,12 +670,19 @@ def task6(EncID=None):
     assert EncID in daily_bloom_filter
     #print(daily_bloom_filter)
     print("[ ======== insert into DBF (murmur3 hashing with 3 hashes) ]")
-    
-    global encID
-    EncID = None
-    if not encID:
-        encID = None
 
+    # Making local EncID = None
+    EncID = None
+    
+    # global encID
+    # EncID = None
+    # if not encID:
+    #     encID = None
+
+
+
+
+############################## TASK 7 ##############################
 # Task 7: 7-A Show that the devices are encoding multiple EncIDs into the same DBF and show the state of the DBF after each addition.
 # Task 7: 7-B Show that a new DBF gets created for the devices after every 10 minutes. A device can only store maximum of 6 DBFs.
 DBF_list = []
@@ -821,7 +849,6 @@ def task10():
     print(data)
 
 #task10()
-task1()
 
 # Task 11: 11-A Show that the device is able to establish a TCP connection with the centralised server and perform Tasks 9 and 10 successfully.
 # Task 11: 11-B Show the terminal for the back-end server performing the QBF-CBF matching operation for risk analysis.
